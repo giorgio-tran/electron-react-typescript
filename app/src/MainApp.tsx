32ce24561ea3f5261f05a3bc032440b4c4c7c89d
Giorgio <tran.giorgio@gmail.com>
2023-05-22 23:17:42 -1000

    Remove choices and reimplement them as buttons

--- app/src/MainApp.tsx
+++ app/src/MainApp.tsx
@@ -1,15 +1,27 @@
-import Choice from "./components/Choice";
+import { useState } from "react";
 import Container from "./components/Container";
 
 const MainApp = () => {
+  const [userChoice, setUserChoice] = useState<string>("");
+  const handleSetUserChoice = (choice: string) => {
+    setUserChoice(choice);
+  }
+
+  const choices = ["rock", "paper", "scissors"];
+
   return (
     <Container>
       <div className='title-xl'>Hello</div>
       <div className="choices-container">
-        <Choice name="rock" />
-        <Choice name="paper" />
-        <Choice name="scissors" />
+        {choices.map((choice, index): JSX.Element => {
+          return (
+            <button key={index} onClick={() => handleSetUserChoice(choice)}>
+              {choice}
+            </button>
+          );
+        })}
       </div>
+      <div>{`Player choice: ${userChoice}`}</div>
     </Container>
   );
 }
