1778e668f5c1164b90e87d32c60bbc1ec63a80d5
Giorgio <tran.giorgio@gmail.com>
2023-05-23 12:24:22 -1000

    Implement rps algorithm

--- app/src/MainApp.tsx
+++ app/src/MainApp.tsx
@@ -3,11 +3,44 @@ import Container from "./components/Container";
 
 const MainApp = () => {
   const [userChoice, setUserChoice] = useState<string>("");
-  const handleSetUserChoice = (choice: string) => {
-    setUserChoice(choice);
+  const [cpuChoice, setCpuChoice] = useState<string>("");
+  const [winner, setWinner] = useState<string>("");
+  
+  const choices = ["rock", "paper", "scissors"];
+
+  const getCpuChoice = () => {
+    const index = Math.floor(Math.random() * 3);
+    const cpuChoice = choices[index];
+    setCpuChoice(cpuChoice);
   }
 
-  const choices = ["rock", "paper", "scissors"];
+  const handleClick = (choice: string) => {
+    setUserChoice(choice);
+    getCpuChoice();
+    determineWinner(choice);
+
+  }
+
+  const determineWinner = (choice: string) => {
+    const index = Math.floor(Math.random() * 3);
+    const cpuChoice = choices[index];
+
+    if (userChoice) {
+      setCpuChoice(cpuChoice);
+    }
+
+    if (choice === choices[index]) {
+      setWinner("neither player");
+    } else if (choice === "rock" && cpuChoice === "paper"
+      || choice === "paper" && cpuChoice === "scissors"
+      || choice === "scissors" && cpuChoice === "rock") {
+      setWinner("CPU");
+    } else if (choice !== "") {
+      setWinner("Player");
+    } else {
+      setWinner("");
+    }
+  }
 
   return (
     <Container>
@@ -15,13 +48,15 @@ const MainApp = () => {
       <div className="choices-container">
         {choices.map((choice, index): JSX.Element => {
           return (
-            <button key={index} onClick={() => handleSetUserChoice(choice)}>
+            <button key={index} onClick={() => {handleClick(choice)}}>
               {choice}
             </button>
           );
         })}
       </div>
       <div>{`Player choice: ${userChoice}`}</div>
+      <div>{`Computer choice: ${cpuChoice}`}</div>
+      <div>{`Round winner: ${winner}`}</div>
     </Container>
   );
 }
